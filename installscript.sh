34713432485a11e95d8707cad7cda4e8ee0a5e48
ipaulo <(None)>
2015-04-08 15:48:24 +0300

    Intermediate commit

--- installscript.sh
+++ installscript.sh
@@ -3,14 +3,44 @@
 # This script is install script that creates symlinks from
 # the home directory to any desired dotfiles in ~/dotfiles
 ############################
+set -e 
+set -x 
+
+
+############################ INSTALL PACKAGES #################################
+pkgs="git vim terminator tmux ipython"
+sudo apt-get update 
+sudo apt-get install $pkgs
+###############################################################################
+
+
+######################### INSTALL VUNDLE FOR VIM  #############################
+src="https://github.com/gmarik/Vundle.vim.git"
+dst="~/.vim/bundle/Vundle.vim"
+test -d $dst || git clone $src $dst
+###############################################################################
+
+
+######################## Install font for terminator ##########################
+FONT_NAME="SourceCodePro"
+URL="https://github.com/adobe-fonts/source-code-pro/archive/1.017R.tar.gz"
+mkdir -p /tmp/$FONT_NAME
+cd /tmp/$FONT_NAME
+wget $URL -O "`echo $FONT_NAME`.tar.gz"
+tar --extract --gzip --file ${FONT_NAME}.tar.gz
+sudo mkdir /usr/share/fonts/truetype/$FONT_NAME
+sudo cp -rf /tmp/$FONT_NAME/. /usr/share/fonts/truetype/$FONT_NAME/.
+fc-cache -f -v
+###############################################################################
+
 
 ########## Variables
-
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files="gitconfig"                 # list of all dotfiles in ~/
 files="$files vimrc"
 files="$files latexmkrc"
+files="$files inputrc"
 
 ##########
 
